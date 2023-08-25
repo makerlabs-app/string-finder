3d3e6ee62ef2d1962f6262172d0ac9848cc47e4e
Kevin Raimbaud <kevin.raimbaud@ext.hermes.com>
2023-08-25 17:54:09 +0200

    Remove comment

--- interfaces.ts
+++ interfaces.ts
@@ -10,22 +10,3 @@ export interface HttpHeaders {
     [key: string]: string;
 }
 
-/*
-
-J'ai cette fonction
-
-async function request(url : string, method : string, headers: any) : Promise<Response> {
-    return await fetch(url, {
-        method: method,
-        headers: headers
-    });
-}
-
-(no-explicit-any) `any` type is not allowed
-async function request(url : string, method : string, headers: any) : Promise<Response> {
-
-
-en sachant que je lui donne des objets qui peuvent contenir ce type de data :
-
-{"Accept-Language": "fr_fr", "X-Locale": "fr_fr"}
-*/
